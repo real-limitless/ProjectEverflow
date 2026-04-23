273f043c6ea09d19d4577686478fceb8305eb0ef
Chen Chiu <chenchiu9@gmail.com>
2026-04-22 21:37:54 -0400

    feat: update Vite configuration to allow hosts for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { Server } from "http";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    allowedHosts: [".everflow.bot"],
     host: "::",
     port: 8080,
     hmr: {
